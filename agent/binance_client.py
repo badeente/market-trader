3101c879165586e27195c8be540a3a9fbc599bee
badeente <badeente.andy@gmail.com>
2024-11-14 22:35:00 +0100

    add binance client tester file (unfinished)

---
+++ agent/binance_client.py
@@ -0,0 +1,6 @@
+from binance.client import Client
+import time
+
+API_KEY = '5f6bde248816576dbd57093f7d5076af181952ecc8056b319d49a21df13d75f4'
+SECRET_KEY = 'cf278b61abf9ff29ab6612038ee0152c42773f4cd638bafb5543088814cf8976'
+
